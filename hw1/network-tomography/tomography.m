8b1be7a05872c938822adc3072a292d2d4613ecd
Michele Laurenti <laurenti.1603064@studenti.uniroma1.it>
2018-03-31 12:31:58 +0200

    Update tomography test.
    Implement rough version of MaxIdentifiable.

--- hw1/network-tomography/tomography.m
+++ hw1/network-tomography/tomography.m
@@ -1,10 +1,8 @@
-nNodes = 10;
-nEdges = 20;
-nMonitors = 5;
+nNodes = 40;
+nEdges = 80;
+nMonitors = 6;
 
 G = makeGraph(nNodes,nEdges);
-
-plot(G);
 
 monitors = randperm(nNodes);
 monitors = monitors(1:nMonitors);
@@ -12,7 +10,9 @@ monitors = monitors(1:nMonitors);
 monitor_couples = nchoosek(monitors,2);
 nPaths = size(monitor_couples,1);
 
-testMatrix = zeros(nNodes, nPaths);
+fprintf("%d paths.\n", nPaths);
+
+testMatrix = zeros(nPaths, nNodes);
 
 paths = [];
 
@@ -25,17 +25,90 @@ for i = 1:nPaths
     end
 end
 
-for path = paths
-    disp(cell2mat(path));
+% for path = paths
+%     disp(cell2mat(path));
+% end
+% disp(testMatrix);
+
+% i=1;
+% 
+% for path = paths
+%     subplot(4,3,i);
+%     H = plot(G);
+%     hold on;
+% 
+%     XData = H.XData;
+%     YData = H.YData;
+% 
+%     path_mat = cell2mat(path);
+%     plot(XData(path_mat), YData(path_mat));
+% 
+%     hold off;
+%     i = i + 1;
+% end
+
+identifiability = zeros(nNodes,nNodes);
+
+identifiability(1,:) = arrayfun(@(i) any(testMatrix(:,i)), 1:nNodes);
+
+nonZero = find(identifiability(1,:));
+
+for k = 1:(nNodes - 1)
+    fprintf("Computing %d identifiability...\n", k);
+    identifiability(k+1,:) = identifiability(k,:);
+    for v = 1:nNodes
+        if identifiability(k+1,v) == 0
+            continue;
+        end
+        A_choice = [1:(v-1) (v+1):nNodes];
+        A_choice = intersect(A_choice, nonZero);
+        B_choice = (v+1):nNodes;
+        B_choice = intersect(B_choice, nonZero);
+        for A = nchoosekSafe(A_choice, k-1)'
+            for B = nchoosekSafe(B_choice, k)'
+                T1 = orReduce(testMatrix,[A' v]);
+                T2 = orReduce(testMatrix,B');
+                if all(T1 == T2)
+                    for v1 = setxor([A; v], B)
+                        identifiability(k+1, v1) = 0;
+                    end
+                end
+            end
+        end
+    end
+    if ~any(identifiability(k+1,:))
+        fprintf("No %d-identifiable nodes, breaking.\n", k);
+        break;
+    end
 end
-disp(testMatrix);
+
+% disp(identifiability);
 
 function G = makeGraph(nNodes, nEdges)
     G = graph();
-    G.addnode(nNodes);
+    G = G.addnode(nNodes);
     edges = nchoosek(1:nNodes, 2);
     chosen = randperm(size(edges,1));
     chosen = chosen(1:nEdges);
     edges = edges(chosen,:);
     G = G.addedge(edges(:,1), edges(:,2));
+end
+
+function R = orReduce(M,C)
+    R = zeros(size(M,1),1);
+    if size(C,1) > 0
+        for c = C
+            R = R | M(:,c);
+        end
+    end
+end
+
+function C = nchoosekSafe(V,k)
+    if size(V,2) == 1
+        C = V;
+    elseif size(V,2) < k
+        C = V;
+    else
+        C = nchoosek(V, k);
+    end
 end
